6a1b50c7ab872e4ed3842a254cde990b4a8c8057
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-09-24 18:58:02 +0200

    todoitem model

---
+++ Model/TodoItem.cs
@@ -0,0 +1,17 @@
+﻿namespace ToDoApplication_RazorPages.Model
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime DateAndTime { get; set; }
+
+        public string Priority { get; set; }
+
+        public bool isDone { get; set; }
+    }
+}
