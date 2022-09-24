c5340b445c403b346bf352bb37766a8f7e56594c
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-09-24 19:31:15 +0200

    app db context

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ToDoApplication_RazorPages.Model;
+
+namespace ToDoApplication_RazorPages.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
