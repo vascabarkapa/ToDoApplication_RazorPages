c1dafe57eac0f2f11ce76b09338b08b81ac92c9d
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-09-24 19:21:08 +0200

    data annotattion on todo item

--- Model/TodoItem.cs
+++ Model/TodoItem.cs
@@ -1,17 +1,25 @@
-﻿namespace ToDoApplication_RazorPages.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApplication_RazorPages.Model
 {
     public class TodoItem
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Description { get; set; }
 
+        [Required]
         public DateTime DateAndTime { get; set; }
 
+        [Required]
         public string Priority { get; set; }
 
+        [Required]
         public bool isDone { get; set; }
     }
 }
