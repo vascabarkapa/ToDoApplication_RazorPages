c732295c74123b2d38251393bce06840aa19f385
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-09-25 13:06:23 +0200

    custom name - data annotation

--- Model/TodoItem.cs
+++ Model/TodoItem.cs
@@ -14,12 +14,14 @@ namespace ToDoApplication_RazorPages.Model
         public string Description { get; set; }
 
         [Required]
+        [Display(Name="Date and Time")]
         public DateTime DateAndTime { get; set; }
 
         [Required]
         public string Priority { get; set; }
 
         [Required]
+        [Display(Name = "Done")]
         public bool isDone { get; set; }
     }
 }
