6cce5167bcdcf63967b63c98b6177508d96e7b33
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-09-25 12:34:52 +0200

    onpost method

--- Pages/TodoItems/Create.cshtml.cs
+++ Pages/TodoItems/Create.cshtml.cs
@@ -1,15 +1,31 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ToDoApplication_RazorPages.Data;
 using ToDoApplication_RazorPages.Model;
 
 namespace ToDoApplication_RazorPages.Pages.TodoItems
 {
     public class CreateModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+
+        public CreateModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public TodoItem TodoItem { get; set; }
 
         public void OnGet()
         {
+        }
+
+        public async Task<IActionResult> OnPost(TodoItem todoItem)
+        {
+            await _db.TodoItems.AddAsync(todoItem);
+            await _db.SaveChangesAsync();
+
+            return RedirectToPage("Index");
         }
     }
 }
