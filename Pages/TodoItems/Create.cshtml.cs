9762ccdc51c717ec294846054fe0c5e4a2ed9681
Vasilije Čabarkapa <vascabarkapa@gmail.com>
2022-09-25 12:22:47 +0200

    create page

--- Pages/TodoItems/Create.cshtml.cs
+++ Pages/TodoItems/Create.cshtml.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ToDoApplication_RazorPages.Model;
 
 namespace ToDoApplication_RazorPages.Pages.TodoItems
 {
     public class CreateModel : PageModel
     {
+        public TodoItem TodoItem { get; set; }
+
         public void OnGet()
         {
         }
